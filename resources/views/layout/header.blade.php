af2f684f13715cb11b8d159b4d9f6f3cb5399964
adinugro88 <adin5337@gmail.com>
2023-08-19 14:07:14 +0700

    update konten

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -22,7 +22,10 @@
                 <div class="header-top-area">
                     <ul class="lab-ul left">
                         <li>
-                            <i class="icofont-ui-call"></i> <span>{{$contact->email}}</span>
+                            <i class="icofont-ui-call"></i> <span> {{$contact->handphone}}</span>
+                        </li>
+                        <li>
+                            <i class="icofont-ui-call"></i> <span>{{$contact->handphone2}}</span>
                         </li>
                         <li>
                             <i class="icofont-location-pin"></i> {{$contact->address}}
@@ -30,11 +33,10 @@
                     </ul>
                     <ul class="lab-ul social-icons d-flex align-items-center">
                         <li><p>Find us on : </p></li>
-                        <li><a href="#" class="fb"><i class="icofont-facebook-messenger"></i></a></li>
-                        <li><a href="#" class="twitter"><i class="icofont-twitter"></i></a></li>
-                        <li><a href="#" class="vimeo"><i class="icofont-vimeo"></i></a></li>
-                        <li><a href="#" class="skype"><i class="icofont-skype"></i></a></li>
-                        <li><a href="#" class="rss"><i class="icofont-rss-feed"></i></a></li>
+                        <li><a href="#" class="fb"><i class="icofont-facebook"></i></a></li>
+                        <li><a href="#" class="twitter"><i class="icofont-instagram"></i></a></li>
+                        <li><a href="#" class="twitter"><i class="icofont-youtube"></i></a></li>
+                        
                     </ul>
                 </div>
             </div>
@@ -50,52 +52,15 @@
                             <ul class="lab-ul">
                                 <li>
                                     <a href="#0">Home</a>
-                                    <ul class="lab-ul">
-                                        <li><a href="index.html" class="active">Home One</a></li>
-                                        <li><a href="index-2.html">Home Two</a></li>
-                                        <li><a href="index-3.html">Home Three</a></li>
-                                        <li><a href="index-4.html">Home Four</a></li>
-                                        <li><a href="index-5.html">Home Five</a></li>
-                                        <li><a href="index-6.html">Home Six</a></li>
-                                        <li><a href="index-7.html">Home Seven</a></li>
-                                    </ul>
                                 </li>
                                 
                                 <li>
+                                    <a href="#0">About</a>
+                                   
+                                </li>
+                                <li>
                                     <a href="#0">Courses</a>
-                                    <ul class="lab-ul">
-                                        <li><a href="course.html">Course</a></li>
-                                        <li><a href="course-single.html">Course Details</a></li>
-    
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#0">Blog</a>
-                                    <ul class="lab-ul">
-                                        <li><a href="blog.html">Blog Grid</a></li>
-                                        <li><a href="blog-2.html">Blog Style 2</a></li>
-                                        <li><a href="blog-3.html">Blog Style 3</a></li>
-                                        <li><a href="blog-single.html">Blog Single</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#0">Pages</a>
-                                    <ul class="lab-ul">
-                                        <li><a href="about.html">About</a></li>
-                                        <li><a href="team.html">Team</a></li>
-                                        <li><a href="instructor.html">Instructor</a></li>
-                                        <li>
-                                            <a href="#0">Shop Pages</a>
-                                            <ul class="lab-ul">
-                                                <li><a href="shop.html">Shop Page</a></li>
-                                                <li><a href="shop-single.html">Shop Details Page</a></li>
-                                                <li><a href="cart-page.html">Shop Cart Page</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="search-page.html">Search Page</a></li>
-                                        <li><a href="search-none.html">Search None</a></li>
-                                        <li><a href="404.html">404</a></li>
-                                    </ul>
+                                    
                                 </li>
                                 <li><a href="contact.html">Contact</a></li>
                             </ul>
