b74ac0768896c936e34dc0835693fefd46d50acc
adinugro88 <adin5337@gmail.com>
2023-08-27 09:34:34 +0700

    update

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="{{setting('site.description')}}">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>{{setting('site.title')}}</title>
 
     <link rel="shortcut icon" href="{{  Voyager::image(setting('site.logo'))}}" type="image/x-icon">
